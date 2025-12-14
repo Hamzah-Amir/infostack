4025330150bfa5458940427e4fcc0e5ad0f815b0
Hamzah-Amir <hamzaamir7651@gmail.com>
2025-12-14 17:11:49 +0500

    adding crud operations

--- app/api/content/route.js
+++ app/api/content/route.js
@@ -1,3 +1,28 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    const articles = await prisma.content.findMany({
+        where: {
+            category: 'sports'
+        }
+    })
+
+    return new Response(JSON.stringify(articles))
+}
+
 export async function POST(req) {
-    
+    await prisma.content.create({
+        data: {
+            title: title,
+            description: desc,
+            category: category,
+            raw_data: {
+                content: article,
+                url: sourceUrl,
+                image: imageUrl,
+                publishedAt: publishedAt
+            },
+        }
+    })
 }
