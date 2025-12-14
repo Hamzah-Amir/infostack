00e4d176ce84c806ba5dcebca277fb97f93b3680
Hamzah-Amir <hamzaamir7651@gmail.com>
2025-12-14 21:05:02 +0500

    completely scrappping full article

--- scripts/fetchNewsJob.js
+++ scripts/fetchNewsJob.js
@@ -1,17 +1,26 @@
+import axios from "axios"
 import { fetchNews } from "../lib/news.js"
+const apiKey = "40c59a6e79fbd98314053bbc6b0a9bb3925a0a77"
 
 async function autoFetchNews() {
     const newsArray = []
     const news = await fetchNews("trending_news", "sports")
     const articles = news.articles
-    console.log(news)
 
     for (let index = 0; index < articles.length; index++) {
         const element = articles[index];
-        console.log("-----------------------------------------\n")
-        console.log(element)
+        newsArray.push(element)
+        const url = element.url
+        const res = await axios.get(`https://extractorapi.com/api/v1/extractor/?apikey=40c59a6e79fbd98314053bbc6b0a9bb3925a0a77&url=${url}`)
+        console.log("\n--------------------\n")
+        const content = res.data.text
+        const article = {
+            element,
+            content: content
+        }
+        console.log(article)
+        newsArray.push(article)
     }
-    
 }
 
 autoFetchNews()
