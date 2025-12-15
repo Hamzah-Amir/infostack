6e1f87710f3f396c92fb18aaef2f3c381fda8d27
Hamzah-Amir <hamzaamir7651@gmail.com>
2025-12-15 07:55:47 +0500

    created migration

---
+++ prisma/migrations/20251215025525_init/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE `Content` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `category` VARCHAR(191) NOT NULL,
+    `source_url` VARCHAR(191) NOT NULL,
+    `source` VARCHAR(191) NOT NULL,
+    `raw_data` JSON NOT NULL,
+    `ai_summary` TEXT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Apis` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `base_url` VARCHAR(191) NOT NULL,
+    `category` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `auth_type` VARCHAR(191) NOT NULL,
+    `example_request` VARCHAR(191) NULL,
+    `example_response` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
