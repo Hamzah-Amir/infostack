a2fc55eb465dd013c2503cfd688ceffbf27f768b
Hamzah-Amir <hamzaamir7651@gmail.com>
2025-12-15 09:48:38 +0500

    fixing errors

--- scripts/fetchNewsJob.js
+++ scripts/fetchNewsJob.js
@@ -1,25 +1,48 @@
 import axios from "axios"
 import { fetchNews } from "../lib/news.js"
+import { prisma } from "../lib/prisma.js"
 const apiKey = "40c59a6e79fbd98314053bbc6b0a9bb3925a0a77"
 
 async function autoFetchNews() {
     const newsArray = []
-    const news = await fetchNews("trending_news", "sports")
+    const category = 'sports'
+    const news = await fetchNews("trending_news", category)
     const articles = news.articles
 
     for (let index = 0; index < articles.length; index++) {
-        const element = articles[index];
-        newsArray.push(element)
-        const url = element.url
-        const res = await axios.get(`https://extractorapi.com/api/v1/extractor/?apikey=40c59a6e79fbd98314053bbc6b0a9bb3925a0a77&url=${url}`)
-        console.log("\n--------------------\n")
-        const content = res.data.text
-        const article = {
-            element,
-            content: content
+        let article = articles[index];
+        if (!article.content) {
+            console.log("Content Not available!")
+            continue
         }
-        console.log(article)
-        newsArray.push(article)
+        const url = article.url
+        const res = await axios.get(`https://extractorapi.com/api/v1/extractor/?apikey=40c59a6e79fbd98314053bbc6b0a9bb3925a0a77&url=${url}`, { timeout: 5000000 })
+        const content = res.data.text
+        const middleIndex = Math.floor(Math.random() * (res.data.images.length - 2)) + 1;
+        const randomMiddleImage = res.data.images[middleIndex];
+        const images = [
+            res.data.images[0],                  // first
+            randomMiddleImage,          // random middle
+            res.data.images[res.data.images.length - 1]   // last
+        ];
+
+
+        const enrichedArticle = {
+            id: article.source.id,
+            title: article.title,
+            description: article.description,
+            category: category,
+            source_url: article.url,
+            source: article.source.name,
+            article: content,
+            images: images
+        }
+        // console.log(enrichedArticle)
+        newsArray.push(enrichedArticle)
+        // const db = await prisma.content.create({
+        //     data:{ article }
+        // })
+        // console.log(db)
     }
 }
 
